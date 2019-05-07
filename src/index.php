4109704e5ece8a6f5bbd071cd8189f8bd93350a1
Marcelle18 <kapm@gmx.com>
2019-05-07 11:14:25 +0200

    [PROJET-WEB][ADD] CSS for contactez_nous.html and php config

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php
+ $db = mysqli_connect('localhost','root@localhost','','emasso')
+ or die('Error connecting to MySQL server.');
+?>
+
+<html>
+ <head>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL</h1>
+</body>
+</html>
