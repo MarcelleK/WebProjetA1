3c9d7aeaed70fd9f440bbfefde9b702bc8a7c22c
Marcelle18 <kapm@gmx.com>
2019-05-07 14:31:49 +0200

    [PROJET-WEB][ADD] Connection to database

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
- $db = mysqli_connect('localhost','root@localhost','','emasso')
+ $db = mysqli_connect('localhost','root','','emasso')
  or die('Error connecting to MySQL server.');
 ?>
 
@@ -8,5 +8,19 @@
  </head>
  <body>
  <h1>PHP connect to MySQL</h1>
+
+<?php
+$query = "SELECT idAdherent, Nom, Prenom, Ville FROM adherent";
+mysqli_query($db, $query) or die('Error querying database.');
+
+$result = mysqli_query($db, $query);
+$row = mysqli_fetch_array($result);
+
+while ($row = mysqli_fetch_array($result)) {
+ echo $row['idAdherent'] . ' ' . $row['Nom'] . ': ' . $row['Prenom'] . ' ' . $row['Ville'];
+
+}
+?>
+
 </body>
 </html>
