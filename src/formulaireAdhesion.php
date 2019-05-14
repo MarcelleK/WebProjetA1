c6dadd75fc14398008c1ca172f18009357336552
Marcelle18 <kapm@gmx.com>
2019-05-14 10:27:04 +0200

    [PROJET-WEB][UPDATE] Connect formulary to database with php

--- src/formulaireAdhesion.php
+++ src/formulaireAdhesion.php
@@ -53,11 +53,12 @@ else{
 <!doctype html>
 <html lang="fr">
 <head>
+    <link rel="icon" href="../img/Logo1.png">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Récupération de vos données</title>
 </head>
 <body>
 <h1>Insertion des contacts</h1>
